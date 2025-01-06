c5567c01862f25f1a8c9eec4653c667e08469767
宇阳 <liuyuyang1024@yeah.net>
2025-01-06 15:30:34 +0800

    1

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -20,6 +20,7 @@ export default async () => {
         {/* 
             为了项目的生态越来越强大，作者在这里恳请大家保留 ThriveX 博客系统版权
             在项目 Star 突破 2K 后大家可自由选择删除 or 保留
+            如果实在想去除该版权，可选择赞助该项目 50 元
         */}
         <div className='py-4 border-t dark:border-black-a transition-colors'>
           <Tooltip showArrow={true} content="一款免费、开源、年轻、高颜值的现代化博客管理系统">
