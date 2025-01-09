722570cdf74e7591d4f7f2e15c485b2acd6d6897
坐公交也用券 <liumou.site@qq.com>
2025-01-10 01:03:48 +0800

    增加NGINX检测,后端地址替换

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -x
+nginx -t
+if [ $? -ne 0 ];then
+    echo "nginx config error"
+    exit 1
+fi
 if [[ -n "${BACKEND_PORT}" ]];then
     export PORT=${BACKEND_PORT}
 fi
@@ -15,6 +20,8 @@ else
   echo "Please set BACKEND_HOST, Use -e BACKEND_HOST=xxxx"
   exit 1
 fi
+# 修改后端地址
+sed -i "s@thrive.Server.demo:9003@${BACKEND_HOST}:${BACKEND_HOST}@" /etc/nginx/conf.d/default.conf
 cd /thrive
 npm run dev&
 # 启动Nginx
