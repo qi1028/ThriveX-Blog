12e2c4d4c7ddf7dccea54ad48bcfbcd623c30880
宇阳 <liuyuyang1024@yeah.net>
2025-01-21 23:10:08 +0800

    完成最新动态组件

--- src/components/ArticleLayout/index.tsx
+++ src/components/ArticleLayout/index.tsx
@@ -1,8 +1,10 @@
 import { getArticlePagingAPI } from '@/api/article'
-import Pagination from "../Pagination"
+import Dynamic from './components/Dynamic'
 import Classics from "./Classics"
 import Waterfall from "./Waterfall"
 import Card from './Card'
+import Pagination from "../Pagination"
+
 import { getConfigDataAPI } from '@/api/project'
 import { Theme } from '@/types/app/project'
 import { Article } from '@/types/app/article'
@@ -17,10 +19,12 @@ export default async ({ page }: { page: number }) => {
 
   return (
     <div className={`w-full md:w-[90%] ${sidebar.length ? 'lg:w-[68%] xl:w-[73%]' : 'w-full'} mx-auto transition-width`}>
+      <Dynamic/>
+      
       {theme.is_article_layout === "classics" && <Classics data={data} />}
       {theme.is_article_layout === "card" && <Card data={data} />}
       {theme.is_article_layout === "waterfall" && <Waterfall data={data} />}
-      
+
       <Pagination total={data?.pages} page={page} className="flex justify-center mt-5" />
     </div>
   )
