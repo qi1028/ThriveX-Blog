cb7c501b92e26c1f00d5be9b6225bc0b2d1dc3c3
宇阳 <liuyuyang1024@yeah.net>
2025-01-24 20:39:19 +0800

    新增底部动物小部件

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Tooltip } from '@nextui-org/react';
 import { getConfigDataAPI } from '@/api/project';
 import { getUserDataAPI } from '@/api/user';
 import { User } from '@/types/app/user';
 import { Web } from '@/types/app/project';
+
+import animals from './images/animals.webp';
 
 export default async () => {
   const { data: user } = await getUserDataAPI() || { data: {} as User }
@@ -11,6 +14,10 @@ export default async () => {
 
   return (
     <>
+      <div className='relative top-[25px] flex justify-center lg:w-[950px] xl:w-[1200px] mx-auto'>
+        <Image src={animals} alt="动物" width={660.34} height={79.99} className='hidden md:block' />
+      </div>
+
       <div className='bg-white dark:bg-black-b border-t dark:border-black-b px-10 transition-colors'>
         <div className="flex justify-center items-center py-4">
           <img src={user?.avatar} alt='作者头像' className='w-20 h-20 rounded-full mr-8 avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.1)]' />
@@ -25,7 +32,7 @@ export default async () => {
         <div className='py-4 border-t dark:border-black-a transition-colors'>
           <Tooltip showArrow={true} content="一款免费、开源、年轻、高颜值的现代化博客管理系统">
             <div className='flex justify-center items-center space-x-3'>
-              <img src="https://bu.dusays.com/2024/11/17/6739adf188f64.png" width={30} height={30} alt='系统LOGO' />
+              <Image src="https://bu.dusays.com/2024/11/17/6739adf188f64.png" width={30} height={30} alt='系统LOGO' />
               <Link href="https://github.com/LiuYuYang01/ThriveX-Blog" target='_blank' className='hover:text-primary transition-colors'> 基于开源项目 ThriveX 构建</Link>
             </div>
           </Tooltip>
