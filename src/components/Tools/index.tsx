0e6eb19a684fea3a758eef98b1172a14b0cdd39a
ZYYO <78634166+ZYYO666@users.noreply.github.com>
2025-02-06 00:26:51 +0800

    Update index.tsx
    层级不够高，会被底部动物的渐变遮挡

--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -21,7 +21,7 @@ export default () => {
 
     return (
         <>
-            <div className="overflow-hidden fixed top-[70%] right-[3%] z-20 flex flex-col w-12 bg-white dark:bg-black-b border dark:border-[#4e5969] rounded-md divide-y dark:divide-[#4e5969] transition-colors">
+            <div className="z-[999] overflow-hidden fixed top-[70%] right-[3%] z-20 flex flex-col w-12 bg-white dark:bg-black-b border dark:border-[#4e5969] rounded-md divide-y dark:divide-[#4e5969] transition-colors">
                 {isDark
                     ? <Image src={sun.src} alt="太阳" width={46} height={46} className={itemSty} onClick={() => setIsDark(false)} />
                     : <Image src={moon.src} alt="月亮" width={46} height={46} className={itemSty} onClick={() => setIsDark(true)} />
