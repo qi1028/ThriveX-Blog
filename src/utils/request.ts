e9d9e1913d601cbf413613fb37e3cf7d8579ff36
坐公交也用券 <liumou.site@qq.com>
2025-02-06 15:58:44 +0800

    修正镜像构建因后端异常导致失败的问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 // 最新调整：在 .env 文件中配置项目后端 API 地址
-const url = "http://thrive.Server.demo:9004"
+const url = "https://api.liuyuyang.net/api"
 // 配置页面缓存时间
 const cachingTime = +process.env.NEXT_PUBLIC_CACHING_TIME!
 
