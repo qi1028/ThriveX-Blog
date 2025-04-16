2242fccdbb58b10d9832e0240d1a5a4e902e8a23
神秘人 <3311118881@qq.com>
2025-04-16 19:48:42 +0800

    更新布局和内容组件的样式，优化过渡效果，确保更流畅的用户体验。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
       {/* 监听路由变化 */}
       <RouteChangeHandler />
 
-      <body id='root' className={`dark:!bg-black-a transition-colors`}>
+      <body id='root' className={`dark:!bg-black-a transition-all`}>
         {/* 🎉 礼花效果 */}
         {/* <Confetti /> */}
 
