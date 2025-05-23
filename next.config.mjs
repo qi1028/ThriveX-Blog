4d5b1f0318d7d2943d170cfe067f7543fabbed43
神秘人 <3311118881@qq.com>
2025-05-23 20:22:20 +0800

    更新 Next.js 配置，替换图片来源配置为远程模式，提升图片加载灵活性和安全性。

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,24 @@ const nextConfig = {
     reactStrictMode: false,
     // 配置图片来源
     images: {
-        domains: [
-            'res.liuyuyang.net',
-            'q1.qlogo.cn',
-            'bu.dusays.com',
+        // domains: [
+        //     'res.liuyuyang.net',
+        //     'q1.qlogo.cn',
+        //     'bu.dusays.com',
+        // ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.liuyuyang.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'q1.qlogo.cn',
+            },
+            {
+                protocol: 'https',
+                hostname: 'bu.dusays.com',
+            }
         ],
     },
 };
