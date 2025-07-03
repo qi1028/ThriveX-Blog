a76fddeb822858c602749932ea00141966075604
神秘人 <3311118881@qq.com>
2025-07-03 16:01:54 +0800

    优化 Empty 组件，替换 Next.js 的 Image 组件为原生 img 标签，以提升性能和兼容性。

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -4,7 +4,8 @@ import EmptySvg from '@/assets/svg/other/empty.svg'
 const Empty = ({ info }: { info: string }) => {
     return (
         <div className="w-52 mx-auto py-12 mt-5">
-            <Image src={EmptySvg} alt="空状态" width={208} height={144} />
+            {/* <Image src={EmptySvg} alt="空状态" width={208} height={144} /> */}
+            <img src={EmptySvg.src} alt="空状态" width={208} height={144} />
             <div className="pt-5 text-center text-gray-700 dark:text-white">{info}</div>
         </div>
     );
